6f521913868e5bd3fcc3f4c1b52e102e747e2ec1
Aleksandr Alekseev <alekseeff_av@mail.ru>
2024-10-06 19:25:07 +0300

    HW-9-04-Reliability in Cloud

---
+++ 9-04-files/main.tf
@@ -0,0 +1,104 @@
+terraform {
+	required_providers {
+		yandex = {
+			source = "yandex-cloud/yandex"
+		}
+	}
+}
+
+provider "yandex" {
+	#token     = "${YC_TOKEN}" # var.yc_iam_token
+    #cloud_id  = var.yc_cloud_id
+    #folder_id = var.yc_folder_id
+    #zone      = var.region
+}
+
+resource "yandex_vpc_network" "network-9-04" {
+	name = "network-9-04"
+}
+
+resource "yandex_vpc_subnet" "subnet-9-04" {
+	name = "subnet-9-04"
+	v4_cidr_blocks = ["172.24.8.0/24"]
+	network_id = yandex_vpc_network.network-9-04.id
+}
+
+resource "yandex_compute_instance" "vm" {
+    count = 2
+	name = "vm${count.index}"
+	platform_id = "standard-v3"
+	scheduling_policy {
+		 preemptible = true
+	}
+	boot_disk {
+		initialize_params {
+			image_id = var.boot_disk_image_id
+			type = "network-hdd"
+			size = 10
+		}
+	}
+	resources {
+		cores  = 2
+		core_fraction = 20
+		memory = 2
+	}
+    network_interface {
+        subnet_id = yandex_vpc_subnet.subnet-9-04.id
+        nat = true
+    }
+    
+    metadata = { 
+        ssh-keys = "yc-user:${file("~/.ssh/yandexssh.pub")}"
+        user-data="${file("user.yml")}" 
+    }
+}
+
+resource "yandex_lb_target_group" "lb-tagretgroup-9-04" {
+    name = "lb-targetgroup"
+    target {
+        subnet_id = "${yandex_vpc_subnet.subnet-9-04.id}"
+        address = yandex_compute_instance.vm[0].network_interface.0.ip_address
+    }
+    target {
+        subnet_id = "${yandex_vpc_subnet.subnet-9-04.id}"
+        address = yandex_compute_instance.vm[1].network_interface.0.ip_address
+    }
+}
+
+resource "yandex_lb_network_load_balancer" "lb-9-04" {
+    name = "lb-9-04"
+    deletion_protection = "false"
+    listener {
+        name = "lb-9-04"
+        port = 80
+        external_address_spec {
+            ip_version = "ipv4"
+        }
+    }
+    attached_target_group {
+        target_group_id = yandex_lb_target_group.lb-tagretgroup-9-04.id
+        healthcheck {
+          name = "http"
+          http_options {
+            port = 80
+            path = "/"
+          }
+        }
+    }
+}
+
+output "lb-9-04-ip" {
+  value = yandex_lb_network_load_balancer.lb-9-04.listener
+}
+
+output "vm-ips" {
+  value = tomap ({
+    for name, vm in yandex_compute_instance.vm : name => vm.network_interface.0.nat_ip_address
+  })
+}
+
+output "vm-ip_addresses" {
+  value = tomap ({
+    for name, vm in yandex_compute_instance.vm : name => vm.network_interface.0.ip_address
+  })
+}
